7028b07c1181c02a3bee027ea6a53ca00ad8ae44
Caroline <caroline.fonseca0001@gmail.com>
2023-03-22 21:50:43 -0400

    worked on exercises

--- app.js
+++ app.js
@@ -1,4 +1,79 @@
 console.log("Hello World!\n==========\n");
 
+let obj = {
+  color: "red",
+  objMethod: function () {
+    console.log(this.color);
+  },
+};
+
+obj;
+
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let Person {
+  constructor (name, pets, residence, hobbies) {
+    this.name = Matt,
+    this.pets = 3,
+    this.residence = Somerville,
+    this.hobbies = travel,
+  }
+  addHobby() {}
+  removeHobby() {}
+  greeting() {
+    console.log("Hello fellow person!");
+  }
+}
+console.log(Person.name);
+
+class Coder {
+  constructor(name, pets, residence, hobbies) {
+    this.name = name;
+    this.pets = pets;
+    this.residence = residence;
+    this.hobbies = hobbies;
+    // super("Caroline", 0, "South Carolina", "dance");
+    this.occupation = "Full Stack Web Developer";
+    greeting();
+    console.log("Hello", Person.name);
+  }
+}
+
+//   age() {
+//     let date = new Date();
+//     return date.getFullYear() - this.year;
+//   }
+
+// let myCar = new Car("Ford", 2014);
+// document.getElementById("demo").innerHTML =
+//   "My car is " + myCar.age() + " years old.";
+
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+
+// class Coder {
+//   constructor(name, pets, residence, hobbies) {
+//     Person.name = name;
+//     Person.pets = pets;
+//     Person.residence = residence;
+//     Person.hobbies = hobbies;
+//   }
+//   addHobby() {}
+//   removeHobby() {}
+//   greeting() {
+//     console.log("Hello fellow person!");
+//   }
+// }
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+let coder = new Person("Matt", 3, "Somerville", [
+  "video games",
+  "travel",
+  "reading",
+]);
+console.log(person);
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
